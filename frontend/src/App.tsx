2ec543d497fb0dffc9f0d184a65e5e85a8a0980f
nitindahiya-dev <nitindahiya00000@gmail.com>
2024-07-28 12:32:53 +0530

    setup project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="text-center">
+      <header className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold text-blue-500">
+          Hello, Tailwind CSS!
+        </h1>
+      </header>
+    </div>
+  );
+}
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customBlue: '#1e40af',
+      },
+    },
+  },
+  plugins: [],
+};
